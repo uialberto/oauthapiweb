e5dc7b12257113ab628575be5ce6cf90e1e73c63
Luis Alberto Baigorria Rodas <uialberto@gmail.com>
2017-06-26 21:16:32 -0400

    www.uibasoft.com - actualizacion packages apiweb ui

---
+++ ApiWebs/ApiWeb/Controllers/DefaultController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ApiWeb.Controllers
+{
+    [RoutePrefix("default")]
+    public partial class DefaultController : AsyncController
+    {
+        // GET: Default
+        [Route("index")]
+        public virtual ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
